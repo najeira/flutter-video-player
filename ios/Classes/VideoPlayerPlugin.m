7546810e7aefbed4808c5210957a0dc65edb7483
najeira <najeira@gmail.com>
2018-08-22 19:18:44 +0900

    fix: allow audio playback in silent mode
    https://github.com/flutter/plugins/pull/624

--- ios/Classes/VideoPlayerPlugin.m
+++ ios/Classes/VideoPlayerPlugin.m
@@ -287,6 +287,9 @@ static void* playbackLikelyToKeepUpContext = &playbackLikelyToKeepUpContext;
 
 - (void)handleMethodCall:(FlutterMethodCall*)call result:(FlutterResult)result {
   if ([@"init" isEqualToString:call.method]) {
+    // Allow audio playback when the Ring/Silent switch is set to silent
+    [[AVAudioSession sharedInstance] setCategory:AVAudioSessionCategoryPlayback error:nil];
+    
     for (NSNumber* textureId in _players) {
       [_registry unregisterTexture:[textureId unsignedIntegerValue]];
       [[_players objectForKey:textureId] dispose];
